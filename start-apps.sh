7cd0199befbd01f71a75e4897d277c50fe523c51
Wellington Souza <wbsouza@gmail.com>
2020-08-09 06:59:30 +0200

    Opening the traefik container ports 80,443 to the external world

--- start-apps.sh
+++ start-apps.sh
@@ -1,12 +1,34 @@
 #!/bin/sh
 
-WEB_NETWORK=$(docker network ls | grep web)
+DOCKER=/usr/bin/docker
+DOCKER_COMPOSE=/usr/local/bin/docker-compose
+
+WEB_NETWORK=$($DOCKER network ls | grep web)
 if [ "${WEB_NETWORK}" == "" ]; then
-  docker network create web
+  $DOCKER network create web
 fi
 
-(cd traefik; docker-compose up -d)
-(cd sonar; docker-compose up -d)
-(cd gitlab; docker-compose up -d)
-(cd plantuml; docker-compose up -d)
+(cd traefik; $DOCKER_COMPOSE up -d)
+(cd sonar; $DOCKER_COMPOSE up -d)
+(cd gitlab; $DOCKER_COMPOSE up -d)
+(cd plantuml; $DOCKER_COMPOSE up -d)
+
+NET_DEVICE=$(ip route | grep default | grep -v tun | awk '{ print $5 }' | sed -z 's/\n//')
+HOST_IP=$(ip route| grep $NET_DEVICE | grep -v default | grep src | awk '{ print $9 }' | sed -z 's/\n//')
+
+TRAEFIK_IP=$(docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" traefik)
+if [ "${TRAEFIK_IP}" == "" ]; then
+  TRAEFIK_IP=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" traefik)
+fi
+
+if [ "${TRAEFIK_IP}" != "" ]; then
+  echo "Exposing the Traefik ports (80, 443) to the external world..."
+  sudo iptables -I FORWARD 1 -s $HOST_IP -d $TRAEFIK_IP -p tcp --dport 80 -j ACCEPT
+  sudo iptables -I FORWARD 1 -s $HOST_IP -d $TRAEFIK_IP -p tcp --dport 443 -j ACCEPT
+  sudo iptables -A FORWARD -d $TRAEFIK_IP -p tcp --dport 80 -j DROP
+  sudo iptables -A FORWARD -d $TRAEFIK_IP -p tcp --dport 443 -j DROP
+else
+  echo ">>> Error trying to get the traefik IP address"
+fi
+
 
